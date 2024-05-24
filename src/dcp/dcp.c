11f1993996f2bef0ddec2a1bbe26876043b0aa56
0913vision <0913vision@gmail.com>
2024-05-24 11:36:41 +0900

    baseline #13

--- src/dcp/dcp.c
+++ src/dcp/dcp.c
@@ -123,6 +123,10 @@ int main(int argc, char** argv)
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    if (rank == 0) {
+       printf("Starting dcp");
+    }
+
     double starttime = MPI_Wtime();
 
     /* pointer to mfu_file src and dest objects */
